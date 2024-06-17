ee494a2e012228ae6145c8135b10764433e720d6
Nitish11K <Nitish.Srivastava@ibm.com>
2024-06-17 16:32:34 +0530

    Add files via upload

---
+++ Python/Activities/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+# @pytest.fixture
+# def num_list():
+    # Create a list of numbers
+ #   int_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    
+    # Alternatively
+    # int_list = list(range(11))
+ 
+#    return int_list
+
+@pytest.fixture
+def wallet_amount():
+   amount = 0
+   return amount
